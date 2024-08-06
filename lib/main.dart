33ec437526afde1d912ba4c1967c03138c046ebd
nrkhan-223 <redoykhan8141@gmail.com>
2024-08-06 19:19:54 +0600

    push-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
-import 'package:flutter/material.dart';
+import 'package:chatgpt/screen/splash_screen/splash_screen.dart';
+import 'consts/export.dart';
+import 'consts/pref.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // hive initialize
+  Pref.initialize();
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ,
+      home: const SplashScreen(),
     );
   }
 }
--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:chatgpt/consts/export.dart';
+import 'package:chatgpt/consts/pref.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,9 +12,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  void initState() {
+    // TODO: implement initState
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    Pref.onboardEnd = true;
 
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: "AiP".text.make(),
+          centerTitle: true,
+        ),
+      ),
     );
   }
 }
